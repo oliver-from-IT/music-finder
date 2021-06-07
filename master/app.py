03dfadce442c7e260651dd88739a36c29bfae830
oliver-from-IT <56422337+oliver-from-IT@users.noreply.github.com>
2021-06-07 14:57:52 +1000

    Added comments
    Provides inline comments and docstrings to the code

--- master/app.py
+++ master/app.py
@@ -15,32 +15,35 @@ from random import randrange
 class Main():
 
     def __init__(self):
-
+        # setup up spotipy authentication
         os.environ['SPOTIPY_CLIENT_ID']='68424f2fdeae4d95a19f0f0e2f1b1221'
         os.environ['SPOTIPY_CLIENT_SECRET']='3545842018874cba81a12e536dbce58e'
 
         auth_manager = SpotifyClientCredentials()
         self.sp = spotipy.Spotify(auth_manager=auth_manager)
 
+        # create XML root tree
         self.root = ET.Element('root')
         self.tree = ET.ElementTree(self.root)
 
+        # put list of genres into dict
         with open('genres.json', 'r') as f:
             genres_dict = json.load(f)
         self.genres_list = genres_dict[0]['genres']
 
     def search_criteria(self, genre):
-
+        '''checks to see if input genre is valid'''
         if genre in self.genres_list:
             return True
         else:
             return False
 
     def get_recommendations(self, required_genres, amount):
-
+        '''uses search criteria to get recommended songs'''
         if required_genres == []:
             return [False, 3, None]
 
+        # for invalid inputs, returns error infomation
         case = 1
         for genre in required_genres:
             if self.search_criteria(genre) == False:
@@ -53,13 +56,16 @@ class Main():
         else:
             return [False, 4, amount]
 
+        # main function to get the recommendations into variable
         print(f'Amount of recommendations: {amount}\nSelected genres: {required_genres}')
         try:
             results = self.sp.recommendations(seed_genres=required_genres, limit=amount)
         except:
             return [True]
 
+        # sorting song data into XML elements
         for tracks in results['tracks']:
+            # song name
             track = str(tracks['name'])
             uri = str(tracks['uri'])
 
@@ -68,18 +74,22 @@ class Main():
 
             artists_list = []
             for artists in tracks['artists']:
+                # song artist/s
                 artist = str(artists['name'])
                 artists_list.append(artist)
 
             ET.SubElement(song, 'artists').text = (', '.join(artists_list))
 
             if tracks['album']['album_type'] == 'ALBUM':
+                # song album
                 album =  tracks['album']['name']
 
                 ET.SubElement(song, 'album').text = album
 
+        # generate XML file with appropriate formatting
         self.tree.write('Recommendations.xml', 'utf-8')
 
+        # return result with prettied XML structure
         print('Search completed')
         try:
             xml = parseString(ET.tostring(self.root))
@@ -93,7 +103,7 @@ class Main():
 class App():
 
     def __init__(self, root):
-        
+        # initialise Tkinter mainframe
         root.title('Music Finder')
         s = ttk.Style()
         s.theme_use('clam')
@@ -104,6 +114,7 @@ class App():
         root.rowconfigure(0, weight=1)
         root.minsize(550, 275)
 
+        # allow scaling of the window size
         mainframe.columnconfigure(0, weight=1)
         mainframe.columnconfigure(1, weight=0)
         mainframe.columnconfigure(2, weight=0)
@@ -112,19 +123,24 @@ class App():
         mainframe.rowconfigure(1, weight=0)
         mainframe.rowconfigure(2, weight=0) 
 
+        # define commands
         output_cmd = root.register(self.get_output)
         validate_cmd = root.register(self.validate)
 
+        # song info box
         self.output_treeview = ttk.Treeview(mainframe, show='tree', height=20)
         self.output_treeview.grid(ipadx=150, column=0, row=0, columnspan=4, sticky='NSEW')
 
+        # scrollbar control for song info box
         output_scrollbar = ttk.Scrollbar(mainframe, orient='vertical', command=self.output_treeview.yview)
         output_scrollbar.grid(column=3, row=0, columnspan=4, sticky='NSE')
         self.output_treeview.configure(yscrollcommand=output_scrollbar.set)
 
+        # get recommendations button
         search_button = ttk.Button(mainframe, text='Get recommendations', command=output_cmd)
         search_button.grid(column=0, row=2, sticky='NSEW')
 
+        # genre inputs with dropdown menu
         self.options_var1 = StringVar()
         self.options_var1.set(Main().genres_list[randrange(1,126)])
         genre_input1 = ttk.Combobox(mainframe, values=Main().genres_list, textvariable=self.options_var1)
@@ -135,19 +151,23 @@ class App():
         genre_input2 = ttk.Combobox(mainframe, values=Main().genres_list, textvariable=self.options_var2)
         genre_input2.grid(column=2, row=2, sticky='NSEW')
 
+        # output amount with entry restrictions to only integers
         self.amount_var = StringVar()
         self.amount_var.set(5)
         output_amount = ttk.Entry(mainframe, textvariable=self.amount_var, validate='key', validatecommand=(validate_cmd, '%P'))
         output_amount.grid(column=3, row=2, sticky='NSEW')
 
+        # define pop-up window conditions for error handling
         self.popup_copy = Menu(self.output_treeview, tearoff=0)
         self.popup_copy.add_command(command=self.copy_selection, label='Copy')
 
+        # bind key shortcuts
         root.bind('<Return>', output_cmd)
         root.bind('<Control-c>', self.copy_menu)
         print(f'GUI set-up done')
 
     def validate(self, input):
+        '''key press validation for output amount'''
         try:
             int(input)
         except ValueError:
@@ -155,6 +175,8 @@ class App():
         return True
 
     def get_output(self):
+        '''sets the song info box with recommended songs'''
+        # puts the selected genres into an array
         selected_genres = []
         option1 = str(self.options_var1.get())
         option2 = str(self.options_var2.get())
@@ -165,6 +187,7 @@ class App():
 
         recommendation_amount = int(self.amount_var.get())
 
+        # performs the main function to get recommended songs with error cases
         recommendations = Main().get_recommendations(selected_genres, recommendation_amount)
         if recommendations[0] == True:
             print(f'Unable to perform search request')
@@ -175,43 +198,48 @@ class App():
             self.display_popup(condition)
             return
 
+        # delete and fill the song info box if applicable
         self.output_treeview.delete(*self.output_treeview.get_children())
-
-        # xml = 'Recommendations.xml'
-        # with open(xml) as f:
-        #     read_data = f.read()
-        # rootNode = ET.fromstring(read_data)
     
         self.create_treeview(recommendations[0])
 
         print(f'Output print finished')
 
     def create_treeview(self, d, depth=0, parent=""):
-
+        '''format the output XML into a treeview for Tkinter'''
+        # set text for each row with the correct depth
         for child in d:
             if depth == 0:
                 text_set = f'{child.tag} uri - {child.attrib["uri"]}'
             else:
                 text_set = f'{child.tag} - {child.text}'
 
+            # inset the row into the treeview display
             item = self.output_treeview.insert(parent, 'end', None, text=text_set, open=True)
 
+            # recursive function for nested rows
             if child.__len__() > 0:
                 self.create_treeview(child, depth + 1, parent=item)
 
     def copy_selection(self):
+        '''allows the user to copy text from the song info box'''
+        # gets the current selected text as a variable
         item = self.output_treeview.selection()[0]
+
+        # clears and sets the clipboard with the selected text
         root.clipboard_clear()
         root.clipboard_append(self.output_treeview.item(item, option='text'))
 
         print(f'Copied: {root.clipboard_get()}')
 
     def copy_menu(self, event):
+        '''creates a small UI menu for copying text'''
         self.output_treeview.identify_row(event.y)
         self.popup_copy.post(event.x_root, event.y_root)
 
     def display_popup(self, condition):
-
+        '''creates a pop-up window to alert the user of errors'''
+        # configures window dimensions
         self.win = Toplevel(padx=5, pady=5, bg='indian red')
         self.win.focus_force()
         self.win.title('Error!')
@@ -220,8 +248,10 @@ class App():
         self.win.rowconfigure(0, weight=1)
         self.win.rowconfigure(1, weight=1)
 
+        # define commands
         resolve_cmd = root.register(lambda : self.resolve_error(condition[0]))
 
+        # sets the text in the label depending on the error condition
         if condition[0] == 3:
             label_text = f'Invalid input: \n\nat least one genre must be picked'
         elif condition[0] == 4:
@@ -229,9 +259,11 @@ class App():
         else:
             label_text = f'Invalid input: \n\ngenre {condition[0]} "{condition[1]}" does not exist'
 
+        # informs the user of what went wrong
         error_label = ttk.Label(self.win, text=label_text)
         error_label.grid(column=0, row=0, sticky='NSEW')
 
+        # function to remove the pop-up and resolve the issue
         resolve_button = ttk.Button(self.win, text='Resolve error', command=resolve_cmd)
         resolve_button.grid(column=0, row=1, sticky='NSEW')
 
@@ -241,7 +273,8 @@ class App():
         self.win.bind('<Return>', resolve_cmd)
 
     def resolve_error(self, condition):
-
+        '''error resolution function for pop-up window'''
+        # returns the appropriate entry field to its default state
         if condition == 1:
             self.options_var1.set(Main().genres_list[randrange(1,126)])
         elif condition == 2:
